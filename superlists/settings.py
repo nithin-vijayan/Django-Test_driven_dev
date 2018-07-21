55a07a002c5284f4a699c32e4d3e6c35d4b33261
Nithin K <nithinkvijayan@icloud.com>
2018-07-21 13:51:52 +0530

    Wrote FT for user specific tests

--- superlists/settings.py
+++ superlists/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+TEST_RUNNER = "redgreenunittest.django.runner.RedGreenDiscoverRunner"
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
