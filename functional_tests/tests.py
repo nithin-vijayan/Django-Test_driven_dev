330a319e99f0e2027f62438c037f562028c7efc7
Nithin K <vijayan@apple.com>
2018-07-24 20:25:42 +0530

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(.5)
 
-	def test_can_start_a_list_and_retrieve_later(self):
+	def test_can_start_a_list_for_one_user(self):
 		self.browser.get(self.live_server_url)
 		
 		self.assertIn('To-Do', self.browser.title)
@@ -47,17 +47,13 @@ class NewVisitorTest(LiveServerTestCase):
 		time.sleep(1)
 
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
-		
-		time.sleep(1)
-		self.browser.get(self.live_server_url)
+				
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
-
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		self.browser.get(self.live_server_url)
